95bc290853125168bcd406f1053c0fb19b8c5e69
kocwng <idakusuma2705@gmail.com>
2023-03-23 01:49:50 +0700

    add entity

---
+++ features/discussions/entity.go
@@ -0,0 +1,24 @@
+package discussions
+
+import (
+	"lapakUmkm/features/products"
+	"time"
+)
+
+type DiscussionEntity struct {
+	Id         uint
+	ProductId  uint
+	Product    products.ProductEntity
+	ParentID   uint
+	Discussion string `validate:"required"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+
+type DiscussionServiceInterface interface {
+	Create(discussionEntity DiscussionEntity) (DiscussionEntity, error)
+}
+
+type DiscussionDataInterface interface {
+	Insert(discussionEntity DiscussionEntity) (uint, error)
+}
