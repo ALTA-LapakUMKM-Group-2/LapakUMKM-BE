c3b2a5588bd2fbd4b26ec684d4b134e6367ca57c
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-23 15:39:53 +0700

    create: cart request

---
+++ features/carts/delivery/request.go
@@ -0,0 +1,7 @@
+package delivery
+
+type CartRequest struct {
+	UserId     uint	`json:"user_id"`
+	ProductId  uint `json:"product_id" form:"product_id"`
+	ProductPcs int  `json:"product_pcs" form:"product_pcs"`
+}
