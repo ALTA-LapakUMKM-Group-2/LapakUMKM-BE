849e7372665a16473bc4c7b5c029f2ecd6c422a9
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-24 15:00:08 +0700

    create: feature mycart

--- features/carts/data/query.go
+++ features/carts/data/query.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"errors"
 	"lapakUmkm/features/carts"
+	"log"
 
 	"gorm.io/gorm"
 )
@@ -24,4 +26,42 @@ func (baq *CartQuery) Add(newCart carts.Core) (carts.Core, error) {
 		return carts.Core{}, tx.Error
 	}
 	return CartToCore(data), nil
+}
+
+// MyCart implements carts.CartData
+func (cq *CartQuery) MyCart(userID uint) ([]carts.Core, error) {
+	tmp := []Cart{}
+	tx := cq.db.Where("carts.user_id = ?", userID).Select("carts.id, carts.user_id, carts.product_id, carts.product_pcs, products.product_name AS product_name, products.price AS product_price, users.full_name AS lapak_name, users.address AS lapak_address").Joins("JOIN users ON carts.user_id = users.id").Joins("JOIN products ON carts.product_id = products.id").Find(&tmp)
+
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	log.Println(tmp)
+	listFeedback := ListCartToCore(tmp)
+	return listFeedback, nil
+}
+
+// Update implements carts.CartData
+func (cq *CartQuery) Update(updateCart carts.Core) (carts.Core, error) {
+	data := CoreToCart(updateCart)
+	tx := cq.db.Model(&Cart{}).Where("id = ? AND user_id = ?", data.ID, data.UserId).Updates(&data)
+	if tx.RowsAffected < 1 {
+		return carts.Core{}, errors.New("no data updated")
+	}
+	if tx.Error != nil {
+		return carts.Core{}, tx.Error
+	}
+	return CartToCore(data), nil
+}
+
+// Delete implements carts.CartData
+func (cq *CartQuery) Delete(userID, cartID uint) error {
+	tx := cq.db.Where("user_id = ?", userID).Delete(&Cart{}, cartID)
+	if tx.RowsAffected < 1 {
+		return errors.New("no data deleted")
+	}
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
 }
--- features/carts/delivery/handler.go
+++ features/carts/delivery/handler.go
@@ -1,7 +1,7 @@
 package delivery
 
 import (
-	// "lapakUmkm/app/middlewares"
+	"lapakUmkm/app/middlewares"
 	"lapakUmkm/features/carts"
 	"lapakUmkm/utils/helpers"
 	"net/http"
@@ -10,30 +10,42 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type BasketHandler struct {
+type CartHandler struct {
 	srv carts.CartService
 }
 
-func New(srv carts.CartService) *BasketHandler {
-	return &BasketHandler{
+func New(srv carts.CartService) *CartHandler {
+	return &CartHandler{
 		srv: srv,
 	}
 }
 
-func (bah *BasketHandler) Add(c echo.Context) error {
-	var formInput CartRequest
+func (ch *CartHandler) Add(c echo.Context) error {
+	var formInput NewCartRequest
 	if err := c.Bind(&formInput); err != nil {
 		return c.JSON(http.StatusBadRequest, helpers.ResponseFail("error bind data"))
 	}
-	// claim := middlewares.ClaimsToken(c)
-	// formInput.UserID = uint(claim.Id)
-	newBasket := carts.Core{}
-	copier.Copy(&newBasket, &formInput)
-	data, err := bah.srv.Add(newBasket)
+	claim := middlewares.ClaimsToken(c)
+	formInput.UserId = uint(claim.Id)
+	newCart := carts.Core{}
+	copier.Copy(&newCart, &formInput)
+	data, err := ch.srv.Add(newCart)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, helpers.ResponseFail(err.Error()))
 	}
 	res := AddResponse{}
 	copier.Copy(&res, &data)
 	return c.JSON(http.StatusCreated, helpers.ResponseSuccess("success add product to cart", res))
+}
+
+func (ch *CartHandler) MyCart(c echo.Context) error {
+	claim := middlewares.ClaimsToken(c)
+	userId := uint(claim.Id)
+	data, err := ch.srv.MyCart(userId)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helpers.ResponseFail(err.Error()))
+	}
+	res := ListCartResponse{}
+	copier.Copy(&res, &data)
+	return c.JSON(http.StatusOK, helpers.ResponseSuccess("success show your cart", res))
 }
