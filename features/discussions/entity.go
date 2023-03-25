049129da1f0e3020ce12185cbbd8b110fdf8f5fb
kocwng <idakusuma2705@gmail.com>
2023-03-25 17:11:39 +0700

    update:entity

--- features/discussions/entity.go
+++ features/discussions/entity.go
@@ -2,6 +2,7 @@ package discussions
 
 import (
 	"lapakUmkm/features/products"
+	"lapakUmkm/features/users"
 	"time"
 )
 
@@ -11,15 +12,27 @@ type DiscussionEntity struct {
 	Product    products.ProductEntity
 	ParentId   uint
 	Discussion string `validate:"required"`
+	UserId     uint
+	User       users.UserEntity
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
 
 type DiscussionServiceInterface interface {
 	Create(discussionEntity DiscussionEntity) (DiscussionEntity, error)
+	Update(discussionEntity DiscussionEntity, id, userId uint) (DiscussionEntity, error)
+	Delete(id, userId uint) error
+	GetFeedbackByProductId(productId uint) ([]DiscussionEntity, error)
+	GetAll() ([]DiscussionEntity, error)
+	GetById(id uint) (DiscussionEntity, error)
 }
 
 type DiscussionDataInterface interface {
-	Insert(discussionEntity DiscussionEntity) (uint, error)
 	SelectById(id uint) (DiscussionEntity, error)
+	Store(discussionEntity DiscussionEntity) (uint, error)
+	Edit(discussionEntity DiscussionEntity, id uint) error
+	Destroy(id uint) error
+	SelectFeedbackByProductId(productId uint) ([]DiscussionEntity, error)
+	SelectAll() ([]DiscussionEntity, error)
+
 }
