23d18466525e0f30ed5922c682d4af47949935f0
kocwng <idakusuma2705@gmail.com>
2023-03-25 20:55:06 +0700

    update:create-handler

--- features/discussions/delivery/handler.go
+++ features/discussions/delivery/handler.go
@@ -1,10 +1,11 @@
 package delivery
 
 import (
+	"lapakUmkm/app/middlewares"
 	"lapakUmkm/features/discussions"
-	// "lapakUmkm/app/middlewares"
 	"lapakUmkm/utils/helpers"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,26 +14,25 @@ type DiscussionHandler struct {
 	Service discussions.DiscussionServiceInterface
 }
 
-func New(srv discussions.DiscussionServiceInterface) *DiscussionHandler{
+func New(srv discussions.DiscussionServiceInterface) *DiscussionHandler {
 	return &DiscussionHandler{
 		Service: srv,
 	}
 }
 
-func (hd *DiscussionHandler) Create(c echo.Context) error{
+func (hd *DiscussionHandler) Create(c echo.Context) error {
 	var formInput DiscussionRequest
 	if err := c.Bind(&formInput); err != nil {
 		return c.JSON(http.StatusBadRequest, helpers.ResponseFail("error bind data"))
 	}
 
-	// claim := middlewares.ClaimsToken(c)
-	// user_id := claim.Id
-	// formInput.UserId = uint(user_id)
+	userId := middlewares.ClaimsToken(c).Id
+	user := DiscussionRequestToDiscussionEntity(&formInput)
+	user.UserId = uint(userId)
 
-	feedback, err := hd.Service.Create(DiscussionRequestToDiscussionEntity(&formInput))
+	discussion, err := hd.Service.Create(user)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, helpers.ResponseFail(err.Error()))
+		return c.JSON(http.StatusBadRequest, helpers.ResponseFail(err.Error()))
 	}
-
-	return c.JSON(http.StatusCreated, helpers.ResponseSuccess("Create Data Success", DiscussionEntityToDiscussionResponse(feedback)))
+	return c.JSON(http.StatusCreated, helpers.ResponseSuccess("Create Data Success", DiscussionEntityToDiscussionResponse(discussion)))
 }
