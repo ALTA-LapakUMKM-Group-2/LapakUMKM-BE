f765a48ac7d2af52aca3ced42c995522b10ee4ca
kocwng <idakusuma2705@gmail.com>
2023-03-27 06:03:59 +0700

    add:model

--- features/productTransactions/data/model.go
+++ features/productTransactions/data/model.go
@@ -1,8 +1,12 @@
 package data
 
 import (
+	"lapakUmkm/features/productTransactions"
+	"lapakUmkm/features/products"
 	dataProduct "lapakUmkm/features/products/data"
+	"lapakUmkm/features/users"
 	dataUser "lapakUmkm/features/users/data"
+	"reflect"
 
 	"gorm.io/gorm"
 )
@@ -18,4 +22,38 @@ type ProductTransaction struct {
 	PaymentLink   string
 	TotalProduct  int
 	TotalPayment  int
+}
+
+func TransactionEntityToTransaction(transactionEntity productTransactions.ProductTransactionEntity) ProductTransaction {
+	return ProductTransaction{
+		UserId:        transactionEntity.Id,
+		ProductId:     transactionEntity.ProductId,
+		TotalProduct:  transactionEntity.TotalProduct,
+		TotalPayment:  transactionEntity.TotalPayment,
+		PaymentStatus: transactionEntity.PaymentStatus,
+		PaymentLink:   transactionEntity.PaymentLink,
+	}
+}
+
+func TransactionToTransactionEntity(transaction ProductTransaction) productTransactions.ProductTransactionEntity {
+	result := productTransactions.ProductTransactionEntity{
+		Id: transaction.ID,
+		UserId: transaction.UserId,
+		ProductId: transaction.ProductId,
+		TotalProduct: transaction.TotalProduct,
+		TotalPayment: transaction.TotalPayment,
+		PaymentStatus: transaction.PaymentStatus,
+		PaymentLink: transaction.PaymentLink,
+	}
+	if !reflect.ValueOf(transaction.User).IsZero() {
+		result.User = users.UserEntity{
+			FullName: transaction.User.FullName,
+		}
+	}
+	if !reflect.ValueOf(transaction.Product).IsZero() {
+		result.Product = products.ProductEntity{
+			ProductName: transaction.Product.ProductName,
+		}
+	}
+	return result
 }
