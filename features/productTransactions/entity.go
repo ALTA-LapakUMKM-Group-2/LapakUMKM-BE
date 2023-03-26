983c49733b9cb4081d742963352f4afe0d68b2c8
kocwng <idakusuma2705@gmail.com>
2023-03-27 04:01:26 +0700

    update:entity

--- features/productTransactions/entity.go
+++ features/productTransactions/entity.go
@@ -1,17 +1,25 @@
 package productTransactions
 
-import "lapakUmkm/features/users"
+import (
+	"lapakUmkm/features/products"
+	"lapakUmkm/features/users"
+)
 
 type ProductTransactionEntity struct {
-	Id     uint
-	UserId uint
-	User   users.UserEntity
-
+	Id            uint
+	UserId        uint
+	User          users.UserEntity
+	ProductId     uint
+	Product       products.ProductEntity
+	TotalProduct  int
+	TotalPayment  int
+	OrderId       string
+	PaymentStatus string
+	PaymentLink   string
 }
 
 type ProductTransactionServiceInterface interface {
 	Create(transctionEntity ProductTransactionEntity) (ProductTransactionEntity, error)
-
 }
 
 type ProductTransactionDataInterface interface {
