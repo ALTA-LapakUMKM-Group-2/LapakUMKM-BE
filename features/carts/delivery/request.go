ff24d815cddb19f28232829aa8af3c8fd27db3e7
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-28 13:15:38 +0700

    add: checkout request

--- features/carts/delivery/request.go
+++ features/carts/delivery/request.go
@@ -1,7 +1,7 @@
 package delivery
 
 type NewCartRequest struct {
-	UserId     uint	`json:"user_id"`
+	UserId     uint `json:"user_id"`
 	ProductId  uint `json:"product_id" form:"product_id"`
 	ProductPcs int  `json:"product_pcs" form:"product_pcs"`
 }
@@ -10,4 +10,8 @@ type UpdateCartRequest struct {
 	Id         uint
 	UserId     uint
 	ProductPcs int `json:"product_pcs" form:"product_pcs"`
+}
+
+type CheckoutRequest struct {
+	Id     []uint `json:"cart_id"`
 }
