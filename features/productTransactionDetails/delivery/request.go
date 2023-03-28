1574212f3caef0f97d99c5e02a4677ba7c4741d3
Findryankp <findryankurnia@gmail.com>
2023-03-28 21:48:29 +0700

    request response

--- features/productTransactionDetails/delivery/request.go
+++ features/productTransactionDetails/delivery/request.go
@@ -1,1 +1,17 @@
 package delivery
+
+import "lapakUmkm/features/productTransactionDetails/data"
+
+type ProductTransactionDetailsRequest struct {
+	ProductTransactionId uint `json:"product_transaction_id" form:"product_transaction_id"`
+	ProductId            uint `json:"product_id" form:"product_id"`
+	TotalProduct         int  `json:"total_product" form:"total_product"`
+}
+
+func RequestToModel(r ProductTransactionDetailsRequest) data.ProductTransactionDetail {
+	return data.ProductTransactionDetail{
+		ProductTransactionID: r.ProductTransactionId,
+		ProductId:            r.ProductId,
+		TotalProduct:         r.TotalProduct,
+	}
+}
--- features/productTransactionDetails/delivery/response.go
+++ features/productTransactionDetails/delivery/response.go
@@ -1,1 +1,40 @@
 package delivery
+
+import (
+	"lapakUmkm/features/productTransactionDetails"
+	"lapakUmkm/features/products/delivery"
+	"reflect"
+)
+
+type ProductTransactionDetailResponse struct {
+	Id                   uint                     `json:"id"`
+	ProductTransactionID uint                     `json:"product_transaction_id"`
+	ProductId            uint                     `json:"product_id"`
+	Product              delivery.ProductResponse `json:"product"`
+	TotalProduct         int                      `json:"total_product"`
+}
+
+func EntityToResponse(e productTransactionDetails.ProductTransactionDetailEntity) ProductTransactionDetailResponse {
+	result := ProductTransactionDetailResponse{
+		Id:                   e.Id,
+		ProductTransactionID: e.ProductTransaction.Id,
+		ProductId:            e.ProductId,
+		TotalProduct:         e.TotalProduct,
+	}
+
+	if !reflect.ValueOf(e.Product).IsZero() {
+		result.Product = delivery.ProductResponse{
+			ProductName: e.Product.ProductName,
+		}
+	}
+
+	return result
+}
+
+func ListEntityToResponse(e []productTransactionDetails.ProductTransactionDetailEntity) []ProductTransactionDetailResponse {
+	var dataResponses []ProductTransactionDetailResponse
+	for _, v := range e {
+		dataResponses = append(dataResponses, EntityToResponse(v))
+	}
+	return dataResponses
+}
