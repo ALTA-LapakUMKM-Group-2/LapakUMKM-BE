d7439f81c99d80303cd6c6917d8170b36e4199d3
Findryankp <findryankurnia@gmail.com>
2023-04-01 09:48:51 +0700

    fix callback midtrans

--- utils/helpers/midtrans.go
+++ utils/helpers/midtrans.go
@@ -25,14 +25,15 @@ func PostMidtrans(data map[string]any) (string, error) {
 }
 
 type ResponseFromCallbackMidtrans struct {
+	TransactionId     string `json:"transaction_id"`
 	TransactionStatus string `json:"transaction_status"`
 	OrderId           string `json:"order_id"`
 	StatusCode        string `json:"status_code"`
 	SignatureKey      string `json:"signature_key"`
+	GrossAmount       string `json:"gross_amount"`
 }
 
 func ValidateSignatureKey(response ResponseFromCallbackMidtrans, orderId, statusCode string) bool {
-	// SHA512(order_id+status_code+gross_amount+ServerKey)
 	str := orderId + statusCode + ServerKey
 	hash := sha512.Sum512([]byte(str))
 	hashStr := hex.EncodeToString(hash[:])
