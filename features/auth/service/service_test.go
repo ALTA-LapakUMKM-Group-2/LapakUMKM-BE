3434ff46489b4a1b6b012893601c116688379f3a
Findryankp <findryankurnia@gmail.com>
2023-04-05 20:52:40 +0700

    test

--- features/auth/service/service_test.go
+++ features/auth/service/service_test.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	"errors"
+	"lapakUmkm/features/auth"
 	"lapakUmkm/features/users"
 	"lapakUmkm/mocks"
 	"lapakUmkm/utils/helpers"
 	"testing"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -29,6 +31,7 @@ func TestLogin(t *testing.T) {
 		Id:       uint(1),
 		Email:    "tes@gmail.com",
 		Password: hash,
+		FullName: "yayan",
 	}
 
 	t.Run("error", func(t *testing.T) {
@@ -39,17 +42,6 @@ func TestLogin(t *testing.T) {
 		assert.ErrorContains(t, err, "user and password not found")
 		repo.AssertExpectations(t)
 	})
-
-	// t.Run("success", func(t *testing.T) {
-	// 	repo.On("GetUserByEmailOrId", "tes@gmail.com", uint(0)).Return(input, nil).Once()
-	// 	srv := New(repo)
-	// 	token, userID, err := srv.Login("tes@gmail.com", "123456")
-	// 	assert.Nil(t, err)
-	// 	assert.NotEmpty(t, token)
-	// 	assert.Equal(t, userID, input.Id)
-	// 	repo.AssertExpectations(t)
-	// })
-
 }
 
 func TestRegister(t *testing.T) {
@@ -92,13 +84,31 @@ func TestRegister(t *testing.T) {
 
 func TestChangePassword(t *testing.T) {
 	repo := mocks.NewAuthDataInterface(t)
-	// pass, _ := helpers.HashPassword("123456")
 	expected := users.UserEntity{
 		Id:       uint(1),
 		FullName: "fin",
 		Email:    "tes@gmail.com",
 		Password: "123456",
 	}
+
+	t.Run("success", func(t *testing.T) {
+		srv := New(repo)
+		password := "password123"
+		hashedPassword, _ := helpers.HashPassword(password)
+
+		repo.On("GetUserByEmailOrId", ".", uint(1)).Return(&users.UserEntity{
+			Id:       1,
+			Email:    "john@example.com",
+			Password: hashedPassword,
+		}, nil)
+
+		repo.On("EditPassword", uint(1), hashedPassword).Return(nil)
+
+		err := srv.ChangePassword(1, password, password, password)
+		assert.NoError(t, err)
+
+		repo.AssertExpectations(t)
+	})
 
 	t.Run("notNull", func(t *testing.T) {
 		srv := New(repo)
@@ -124,16 +134,6 @@ func TestChangePassword(t *testing.T) {
 		assert.ErrorContains(t, err, "old password not match with exist password")
 		repo.AssertExpectations(t)
 	})
-
-	// t.Run("success", func(t *testing.T) {
-	// 	repo.On("GetUserByEmailOrId", ".", uint(1)).Return(expected, nil).Once()
-	// 	pass, _ := helpers.HashPassword("123456")
-	// 	repo.On("EditPassword", uint(1), pass).Return(nil).Once()
-	// 	srv := New(repo)
-	// 	err := srv.ChangePassword(uint(1), "123456", "123456", "123456")
-	// 	assert.NoError(t, err)
-	// 	repo.AssertExpectations(t)
-	// })
 }
 
 func TestIsUserExist(t *testing.T) {
@@ -156,21 +156,39 @@ func TestIsUserExist(t *testing.T) {
 	})
 }
 
-func TestForgetPasswords(t *testing.T) {
+func TestForgetPassword(t *testing.T) {
+	email := "haha@mail.com"
+	token := helpers.EncryptText(email)
+	urlLink := "https://lapakumkm.netlify.app/new-password?token=" + token
+
 	repo := mocks.NewAuthDataInterface(t)
 
-	email := "haha@mail.com"
+	// t.Run("success", func(t *testing.T) {
+	// 	user := users.UserEntity{
+	// 		Id:       uint(123),
+	// 		Email:    email,
+	// 		Password: "hashedpassword",
+	// 		// add other relevant fields as needed
+	// 	}
+	// 	repo.On("GetUserByEmailOrId", email, uint(0)).Return(user, nil).Once()
+	// 	srv := New(repo)
+	// 	err := srv.ForgetPassword(email)
+	// 	assert.NoError(t, err)
+
+	// 	errSendMail := helpers.SendMail("Forget Password", email, urlLink)
+	// 	assert.NoError(t, errSendMail)
+
+	// 	repo.AssertExpectations(t)
+	// })
+
 	t.Run("notExist", func(t *testing.T) {
 		repo.On("GetUserByEmailOrId", "haha@mail.com", uint(0)).Return(users.UserEntity{}, errors.New("email not found")).Once()
 		srv := New(repo)
-		err := srv.IsUserExist("haha@mail.com")
+		err := srv.ForgetPassword("haha@mail.com")
 		assert.NotEmpty(t, err)
 		assert.ErrorContains(t, err, "email not found")
 		repo.AssertExpectations(t)
 	})
-
-	token := helpers.EncryptText(email)
-	urlLink := "https://lapakumkm.netlify.app/new-password?token=" + token
 
 	t.Run("errSendEmail", func(t *testing.T) {
 		repo.On("GetUserByEmailOrId", email, uint(0)).Return(users.UserEntity{}, nil).Once()
@@ -184,7 +202,28 @@ func TestForgetPasswords(t *testing.T) {
 }
 
 func TestNewPassword(t *testing.T) {
+
+	mockData := users.UserEntity{
+		Id:       uint(1),
+		Email:    helpers.EncryptText("test@example.com"),
+		Password: "",
+	}
 	repo := mocks.NewAuthDataInterface(t)
+
+	t.Run("success", func(t *testing.T) {
+		pass := "newpassword"
+		hash, _ := helpers.HashPassword(pass)
+		mockData.Password = hash
+		srv := New(repo)
+		email := helpers.DecryptText(mockData.Email)
+		repo.On("GetUserByEmailOrId", email, uint(0)).Return(mockData, nil).Once()
+		repo.On("EditPassword", uint(1), mock.AnythingOfType("string")).Return(nil).Once()
+		srv.NewPassword(mockData.Email, pass, pass)
+		repo.AssertExpectations(t)
+
+		err := helpers.CheckPasswordHash(pass, hash)
+		assert.Equal(t, err, true)
+	})
 	t.Run("val", func(t *testing.T) {
 		pass := "123456"
 		srv := New(repo)
@@ -241,4 +280,36 @@ func TestLoginSSOGoogle(t *testing.T) {
 	// // 	assert.ErrorContains(t, err, "required")
 	// // 	repo.AssertExpectations(t)
 	// // })
+}
+
+func Test_authService_ChangePassword(t *testing.T) {
+	type fields struct {
+		data     auth.AuthDataInterface
+		validate *validator.Validate
+	}
+	type args struct {
+		id             uint
+		oldPassword    string
+		newPassword    string
+		confirmPssword string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &authService{
+				data:     tt.fields.data,
+				validate: tt.fields.validate,
+			}
+			if err := u.ChangePassword(tt.args.id, tt.args.oldPassword, tt.args.newPassword, tt.args.confirmPssword); (err != nil) != tt.wantErr {
+				t.Errorf("authService.ChangePassword() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
--- go.mod
+++ go.mod
@@ -10,11 +10,14 @@ require (
 )
 
 require (
+	github.com/cweill/gotests v1.6.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.27.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
 
@@ -77,7 +80,7 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.7.0
 	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gorm.io/driver/mysql v1.4.7
